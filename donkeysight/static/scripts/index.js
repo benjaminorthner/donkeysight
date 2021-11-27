e237928b2fdb1374939041454cd8990db770ea68
Benjamin Orthner <benjaminorthner@gmail.com>
2021-11-27 17:26:57 +0100

    started working on CSS Grid implementation

--- donkeysight/static/scripts/index.js
+++ donkeysight/static/scripts/index.js
@@ -3,10 +3,10 @@
 // Create a configuration object that you pass to the generate music function
 var generate_music_defaults = {
     n : 1000,
-    min: 13,
-    max: 80,
+    min: LOToPiano("c/4"),
+    max: LOToPiano("c/6"),
     scale: 4, // Cmaj
-    max_interval: 88,
+    max_interval: 88
 }
 
 
--- donkeysight/static/scripts/render_score.js
+++ donkeysight/static/scripts/render_score.js
@@ -1,14 +1,15 @@
 const VF = Vex.Flow;
 
 // Create an SVG renderer and attach it to the DIV element named "vf".
-const div = document.getElementById("music-block");
+const div = document.getElementById("music-box");
 
 const renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
 
 // Configure the rendering context.
-const block_height = div.offsetHeight;
-const block_width = div.offsetWidth;
-renderer.resize(700, 100);
+const box_height = div.offsetHeight;
+const box_width = div.offsetWidth;
+console.log(box_height)
+renderer.resize(box_width, box_height);
 
 const context = renderer.getContext();
 context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
@@ -18,9 +19,9 @@ context.scale(1,1); //size
 context.setFillStyle('var(--text-color');
 context.setStrokeStyle('var(--text-color');
 
-// Create a stave of width 460 at position 0, 0 on the canvas.
+// Create a stave of width box_width at position 0, 0 within the div.
 // Set color when stave is created (stave isnt connected to defaults for some reason)
-const stave = new VF.Stave(0, 0, 700, {fill_style: 'var(--sub-color)'});
+const stave = new VF.Stave(0, 0, box_width, {fill_style: 'var(--sub-color)'});
 
 // Add a clef and time signature.
 stave.addClef("treble").addTimeSignature("4/4");
@@ -33,9 +34,7 @@ stave.setContext(context).draw();
 let note_count = 10
 let note_list = generate_music({
     n : note_count,
-    min : 50,
-    max : 60,
-    scale : 2})
+    max_interval: 6});
 
 // converting notes to list of vexflow StaveNote objects
 var notes = []
