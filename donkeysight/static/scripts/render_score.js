ff4908fcd9b5ce9114e810321f6be169ff2fbabb
Benjamin Orthner <benjaminorthner@gmail.com>
2021-11-28 14:43:44 +0100

    subgrid for music and vertical centering

--- donkeysight/static/scripts/render_score.js
+++ donkeysight/static/scripts/render_score.js
@@ -20,7 +20,11 @@ context.setStrokeStyle('var(--text-color');
 
 // Create a stave of width box_width at position 0, 0 within the div.
 // Set color when stave is created (stave isnt connected to defaults for some reason)
-const stave = new VF.Stave(0, 0, box_width, {fill_style: 'var(--sub-color)'});
+let stave = new VF.Stave(0, 0, box_width, {fill_style: 'var(--sub-color)'});
+
+// vertically center stave in div 
+// find VF.Stave properties here https://github.com/0xfe/vexflow/blob/master/src/stave.ts
+stave.setY((box_height - stave.getBottomY()) / 2)
 
 // Add a clef and time signature.
 stave.addClef("treble").addTimeSignature("4/4");
@@ -44,7 +48,7 @@ for (let i = 0; i < note_list.length; i++) {
 var voice = new VF.Voice({num_beats: note_count, beat_value: 4});
 voice.addTickables(notes);
 
-var formatter = new VF.Formatter().joinVoices([voice]).format([voice], 600);
+var formatter = new VF.Formatter().joinVoices([voice]).format([voice], box_width);
 
 voice.draw(context, stave);
 
