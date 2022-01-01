0df72782ef6adc7055f8ea5a5db603cc12b652f7
Benjamin Orthner <benjaminorthner@gmail.com>
2022-01-01 23:45:10 +0100

    implemented basic cdml suggestions

--- static/scripts/commandline.js
+++ static/scripts/commandline.js
@@ -20,6 +20,7 @@ function showCommandLine() {
     // set focus on command line and clear command line
     $("#commandLine input").val("");
     $("#commandLine input").focus();
+    updateSuggested();
 }
 
 
@@ -31,4 +32,36 @@ function updateSuggested() {
         .split(" ")
         .filter((s, i) => s || i == 0); //remove empty entries after first
 
+    // access dict that contains all lowest level commands/settings
+    let list = currentCommandLineList[currentCommandLineList.length - 1]
+    
+    // if no input is given yet mark all commands as found
+    if (inputVal[0] === "") {
+        $.each(list.list, (index, obj) => {
+            obj.found = true;
+        });
+    }
+    showFound();
+}
+
+
+// shows all commands which have the found property set to true
+function showFound() {
+
+    // collects html code to display found suggestions
+    let commandsHTML = ''
+    let list = currentCommandLineList[currentCommandLineList.length - 1]
+
+    // loop over each command
+    $.each(list.list, (index, obj) => {
+        // if an object is marked as found generate its HTML code
+        if (obj.found) {
+            // in the future iconHTML will contain the html code for the icon
+            iconHTML = `<div class="icon"><div class="textIcon">!?</div></div>`;
+            commandsHTML += `<div class="entry" command="${obj.id}">${iconHTML}<div>${obj.display}</div></div>`;
+        }
+    });
+
+    // add html to suggestions element
+    $("#commandLine .suggestions").html(commandsHTML);
 }
